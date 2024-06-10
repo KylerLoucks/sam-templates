b9783565141f5673d8e833f428a0cb3e8b20aa4b
cloud303-kloucks <kloucks@cloud303.io>
2024-06-10 09:15:04 -0700

    implement snapshot cleanup lambda

---
+++ stepfunctions/mysql-ephemeral/lambdas/cleanUpOldSnapshots/app.py
@@ -0,0 +1,37 @@
+
+import boto3
+import os
+
+
+ec2 = boto3.client('ec2')
+
+# Amount of snapshots to retain
+SNAPSHOT_RETAIN_AMOUNT = os.environ['SNAPSHOT_RETAIN_AMOUNT']
+
+def lambda_handler(event, context):
+    
+    # Get all snapshots with ephemeral-mysql tag key
+    snapshots: list = ec2.describe_snapshots(
+        Filters=[
+            {
+                'Name': 'tag-key',
+                'Values': ['ephemeral-mysql']
+            }
+        ]
+    )['Snapshots']
+
+    # Sort snapshots by start time in descending order
+    snapshots.sort(key=lambda x: x['StartTime'], reverse=True)
+
+    # Get snapshots to delete (all except the amount specified)
+    snapshots_to_delete = snapshots[SNAPSHOT_RETAIN_AMOUNT:]
+
+    for snapshot in snapshots_to_delete:
+        snapshot_id = snapshot['SnapshotId']
+        print(f"Deleting snapshot {snapshot_id}")
+        ec2.delete_snapshot(SnapshotId=snapshot_id)
+
+    return {
+        'statusCode': 200,
+        'body': f"Deleted {len(snapshots_to_delete)} snapshots"
+    }
